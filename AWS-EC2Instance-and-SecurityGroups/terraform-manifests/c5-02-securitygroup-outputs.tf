f678b7d921cba3d0969a52f02d45d08db9758394
Pankaj Poddar <ppoddar15@gmail.com>
2024-07-21 20:48:37 +0530

    updated AWS-EC2Instance-and-SecurityGroups

---
+++ AWS-EC2Instance-and-SecurityGroups/terraform-manifests/c5-02-securitygroup-outputs.tf
@@ -0,0 +1,33 @@
+## Bastion SG OUTPUT
+
+output "public_bastion_sg_security_group_id" {
+  description = "The ID of the Bastion security group"
+  value       = module.public_bastion_sg.security_group_id
+}
+
+output "public_bastion_sg_security_group_name" {
+  description = "The name of the Bastion security group"
+  value       = module.public_bastion_sg.security_group_name
+}
+
+output "public_bastion_sg_security_group_vpc_id" {
+  description = "The VPC ID of the Bastion SG "
+  value       = module.public_bastion_sg.security_group_vpc_id
+}
+
+
+##Private SG OUTOUT
+output "private_sg_security_group_id" {
+  description = "The ID of the Priivate security group"
+  value       = module.private_sg.security_group_id
+}
+
+output "private_sg_security_group_name" {
+  description = "The name of the Private security group"
+  value       = module.private_sg.security_group_name
+}
+
+output "private_sg_security_group_vpc_id" {
+  description = "The VPC ID of the Private SG "
+  value       = module.private_sg.security_group_vpc_id
+}
